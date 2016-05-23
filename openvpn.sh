518bd39801008cc716ed40d89c36d23e3ecc3a30
Naypam <github@vaunt.eu>
2016-05-23 19:33:12 +0100

    OpenVPN helper zsh script and aws config example

---
+++ openvpn.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+if [[ ! $2 ]]; then
+	echo "Usage: ./openvpn.sh ip port"
+	exit
+fi
+
+ret=`host $1`
+if [[ $? != 0 ]]; then
+	echo "Invalid host $1"
+	exit
+fi
+
+sudo openvpn --remote $1 $2 --config aws2.conf
